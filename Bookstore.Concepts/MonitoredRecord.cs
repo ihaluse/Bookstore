09b7412d65f07dc37789a48d7e6fce440a4883ea
ihaluse <98879089+ihaluse@users.noreply.github.com>
2022-02-07 08:50:08 +0100

    Extending DSL

---
+++ Bookstore.Concepts/MonitoredRecord.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using Rhetos.Dsl;
+using Rhetos.Dsl.DefaultConcepts;
+
+using System;
+using Rhetos.Compiler;
+using Rhetos.Dom.DefaultConcepts;
+using Rhetos.Extensibility;
+
+namespace Bookstore.Concepts
+{
+    [Export(typeof(IConceptInfo))]
+    [ConceptKeyword("MonitoredRecords")]
+    public class MonitoredRecordsInfo : IConceptInfo
+    {
+        [ConceptKey]
+        public EntityInfo Entity { get; set; }
+    }
+
+    [Export(typeof(IConceptMacro))]
+    public class MonitoredRecordsMacro : IConceptMacro<MonitoredRecordsInfo>
+    {
+        public IEnumerable<IConceptInfo> CreateNewConcepts(MonitoredRecordsInfo conceptInfo, IDslModel existingConcepts)
+        {
+            var newConcepts = new List<IConceptInfo>();
+
+            var createdAtPropertyInfo = new DateTimePropertyInfo
+            {
+                Name =  "CreatedAt",
+                DataStructure = conceptInfo.Entity
+            };
+
+            newConcepts.Add(createdAtPropertyInfo);
+            newConcepts.Add(new CreationTimeInfo
+            {
+                Property = createdAtPropertyInfo,
+            });
+            newConcepts.Add(new DenyUserEditPropertyInfo
+            {
+                Property = createdAtPropertyInfo,
+            });
+
+            var loggingProperty = new EntityLoggingInfo
+            {
+                Entity = conceptInfo.Entity
+            };
+
+            newConcepts.Add(loggingProperty);
+
+            newConcepts.Add(new AllPropertiesLoggingInfo
+            {
+                EntityLogging = loggingProperty
+            });
+
+            return newConcepts;
+        }
+    }
+}
---
+++ Bookstore.Concepts/PhoneNumber.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using Rhetos.Dsl;
+using Rhetos.Dsl.DefaultConcepts;
+
+namespace Bookstore.Concepts
+{
+    [Export(typeof(IConceptInfo))]
+    [ConceptKeyword("PhoneNumber")]
+    public class PhoneNumberInfo : ShortStringPropertyInfo
+    {
+    }
+
+    [Export(typeof(IConceptMacro))]
+    public class PhoneNumberMacro : IConceptMacro<PhoneNumberInfo>
+    {
+        public IEnumerable<IConceptInfo> CreateNewConcepts(PhoneNumberInfo conceptInfo, IDslModel existingConcepts)
+        {
+            var newConcepts = new List<IConceptInfo>();
+
+            if (conceptInfo.DataStructure is IWritableOrmDataStructure) // Activate validation only on writable data, for example on Entity.
+                newConcepts.Add(new RegExMatchInfo // Effect is same as adding "RegExMatch" validation on this property in DSL script.
+                {
+                    Property = conceptInfo,
+                    RegularExpression = @"[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*",
+                    ErrorMessage = "Invalid phone number format."
+                });
+
+            return newConcepts;
+        }
+    }
+}
